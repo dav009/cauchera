2bcee5ee4df27f8a90550cb90222c0d64d279bc9
David Przybilla <dav.alejandro@gmail.com>
2020-06-17 21:23:59 +0900

    entity grouping fix

--- fishingrod/ner.py
+++ fishingrod/ner.py
@@ -19,15 +19,20 @@ def is_sent_too_long_for_ner(text):
 def join_b_i_tags(entities):
     """
     fix over hugging face grouped entities bug
-    """
+    # remove this once https://github.com/huggingface/transformers/issues/5077
+    # and https://github.com/huggingface/transformers/issues/4816
+    """ 
     last_tag = ""
     current_entity = ""
+    entity_type = ""
     output_entities = []
     for i in entities:
         current_tag = i["entity"]
         if current_tag.startswith("B"):
             if current_entity:
-                output_entities.append(current_entity)
+                output_entities.append((current_entity, entity_type))
+            entity_type = current_tag.split("-")[1]
+            
             current_entity = i["word"]
             if current_entity.startswith("##"):
                 current_entity = current_entity.replace("##", "")
@@ -39,8 +44,7 @@ def join_b_i_tags(entities):
                 current_entity = current_entity + " " + i["word"]
             last_tag = current_tag
     if last_tag:
-        output_entities.append(current_entity)
-    print(entities)
+        output_entities.append((current_entity, entity_type))
     return output_entities
 
 
